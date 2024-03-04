ec4d45d8485a2f61a64d2ef1daf5d246fb31f5d7
Krasin1 <dunaevv84@gmail.com>
2024-03-04 20:09:24 +0300

    polybar remove underline

--- polybar/.config/polybar/gpu-temp.sh
+++ polybar/.config/polybar/gpu-temp.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-temp="$(nvidia-smi -q -d temperature | awk 'NR==11{print $5}')"
-echo -e "$temp°C"
+if command -v nvidia-smi &> /dev/null; then
+    temp="$(nvidia-smi -q -d temperature | awk 'NR==11{print $5}')"
+    echo -e "$temp°C"
+else
+    echo ""; exit 0;
+fi
