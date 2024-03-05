b5f0da70bb3ec3d623885a2d1edcc22982a24bef
Krasin1 <dunaevv84@gmail.com>
2024-03-05 22:09:02 +0300

    polybar auto cpu temp path + nvim markdown-preview

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,6 +6,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Cpu temp path
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Package id 0" ]; then
+        export HWMON_PATH="$i"
+    fi
+
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "amdgpu:" ]; then
+        export HWMON_PATH="$i"
+    fi
+done
+
 # Launch bar
 polybar example -r &
 
