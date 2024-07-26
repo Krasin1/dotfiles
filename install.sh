945010895ffe1b136a7e2b6f570b84605eb5d0c6
Krasin1 <dunaevv84@gmail.com>
2024-07-26 20:03:12 +0300

    touchpad setup

--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@ while true; do
     read -p "Dotfiles on desktop(yes) or on laptop(no)? : " yn
     case $yn in
         [Yy]* ) 
-            stow bspwm chromium clang-format clangd git kitty nvim picom polybar ranger sxhkd wallpapers x zathura zsh zshrc;
+            stow bspwm chromium clang-format clangd git kitty nvim picom polybar ranger sxhkd wallpapers x zathura zsh zshrc rofi;
             sudo stow -t /etc/X11/xorg.conf.d/ keyboardLayout;
             echo "done";
             break;;
         [Nn]* ) 
-            stow bspwm chromium clang-format clangd git kitty nvim picom polybar ranger sxhkd wallpapers x zathura zsh zshrc xmodmap;
+            stow bspwm chromium clang-format clangd git kitty nvim picom polybar ranger sxhkd wallpapers x zathura zsh zshrc rofi xmodmap;
             sudo stow -t /etc/X11/xorg.conf.d/ keyboardLayout;
+            sudo stow -t /etc/X11/xorg.conf.d/ touchpad;
             echo "done";
             exit;;
         * ) echo "Please answer yes or no.";;
