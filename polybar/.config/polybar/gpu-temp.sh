c0cc496bc5239fb57c413378719005b46b534171
Krasin1 <dunaevv84@gmail.com>
2024-08-28 21:09:45 +0300

    NEW config (very rice)

--- polybar/.config/polybar/gpu-temp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if command -v nvidia-smi &> /dev/null; then
-    temp="$(nvidia-smi -q -d temperature | awk 'NR==11{print $5}')"
-    echo -e "$temp°C"
-else
-    echo ""; exit 0;
-fi
--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,7 +4,7 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
 # Cpu temp path
 for i in /sys/class/hwmon/hwmon*/temp*_input; do 
@@ -12,15 +12,22 @@ for i in /sys/class/hwmon/hwmon*/temp*_input; do
         export HWMON_PATH="$i"
     fi
 
-    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "k10temp: Tctl" ]; then
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "amdgpu: edge" ]; then
         export HWMON_PATH="$i"
     fi
+    
+    # if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "k10temp: Tctl" ]; then
+    #     export HWMON_PATH="$i"
+    # fi
 done
 
-# Launch bar
-polybar example -r &
+for mon in $(polybar --list-monitors | cut -d":" -f1); do
+    (MONITOR=$mon polybar -q pam1 -c ~/.config/polybar/config.ini) &
+    (MONITOR=$mon polybar -q pam2 -c ~/.config/polybar/config.ini) &
+    (MONITOR=$mon polybar -q pam3 -c ~/.config/polybar/config.ini) &
+    (MONITOR=$mon polybar -q pam4 -c ~/.config/polybar/config.ini) &
+    (MONITOR=$mon polybar -q pam5 -c ~/.config/polybar/config.ini) &
+    (MONITOR=$mon polybar -q pam6 -c ~/.config/polybar/config.ini) &
+done
 
-if [[ $(xrandr | grep "DP-2 connected") ]]; then
-    polybar second -r &
-fi
 
