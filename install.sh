aa121fb0b1a0b25436466aa3da778fdfcae2bd17
Krasin1 <dunaevv84@gmail.com>
2024-10-27 20:33:50 +0300

    modify install.sh to install only laptop settings

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 while true; do
-    read -p "Dotfiles on desktop(yes) or on laptop(no)? : " yn
+    read -p "Install keyboard and touchpad settings? : " yn
     case $yn in
         [Yy]* ) 
-            stow bspwm chromium clang-format clangd git kitty nvim picom polybar ranger sxhkd wallpapers x zathura zsh zshrc rofi;
-            sudo stow -t /etc/X11/xorg.conf.d/ keyboardLayout;
-            echo "done";
-            break;;
-        [Nn]* ) 
-            stow bspwm chromium clang-format clangd git kitty nvim picom polybar ranger sxhkd wallpapers x zathura zsh zshrc rofi xmodmap;
+            # stow bspwm chromium clang-format clangd git kitty nvim picom polybar ranger sxhkd wallpapers x zathura zsh zshrc rofi xmodmap;
             sudo stow -t /etc/X11/xorg.conf.d/ keyboardLayout;
             sudo stow -t /etc/X11/xorg.conf.d/ touchpad;
             echo "done";
+            break;;
+        [Nn]* ) 
+            echo "exit"
             exit;;
         * ) echo "Please answer yes or no.";;
     esac
