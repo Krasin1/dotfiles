dce611f0d422840ab37acfb20cce253756931407
Krasin1 <dunaevv84@gmail.com>
2025-09-25 22:34:49 +0300

    nvim transparency add

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -6,9 +6,14 @@ return {
         priority = 1000,
         config = function()
             require("tokyonight").setup({
+                transparent = true,
+                styles = {
+                    floats = "transparent",
+                    sidebars = "transparent"
+                },
                 on_highlights = function (highlights, _)
                     highlights.LspSignatureActiveParameter.bg = "#275378"
-                end
+                end,
             })
             vim.cmd.colorscheme "tokyonight-night"
         end
