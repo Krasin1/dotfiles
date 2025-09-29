8b7f55b9eb39f8110d3c738ba066e27229644b24
Krasin1 <dunaevv84@gmail.com>
2025-09-29 12:46:31 +0300

    eww fix autodetect monitor script

--- eww/.config/eww/scripts/monitor.sh
+++ eww/.config/eww/scripts/monitor.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+
+pidfile="/tmp/eww-monitor-handler.pid"
+
+# Если PID-файл существует и процесс живой → ничего не делаем
+if [[ -f "$pidfile" ]]; then
+    oldpid=$(cat "$pidfile")
+    if ps -p "$oldpid" -o comm= | grep -q "^monitor.sh$"; then
+        echo "Monitor.sh уже запущен (PID $oldpid), выходим"
+        exit 0
+    fi
+fi
+# Записываем новый PID
+echo $$ > "$pidfile"
+
+cleanup() { rm -f /tmp/eww-monitor-handler.pid; }
+trap cleanup EXIT
 
 function handle {
   if [[ ${1:0:12} == "monitoradded" ]]; then
@@ -8,4 +24,4 @@ function handle {
   fi
 }
 
-socat - UNIX-CONNECT:/tmp/hypr/$(echo $HYPRLAND_INSTANCE_SIGNATURE)/.socket2.sock | while read line; do handle $line; done
+socat -U - UNIX-CONNECT:$(echo $XDG_RUNTIME_DIR)/hypr/$(echo $HYPRLAND_INSTANCE_SIGNATURE)/.socket2.sock | while read line; do handle $line; done
