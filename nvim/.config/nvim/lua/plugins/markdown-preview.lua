adc0ea85c01dcb0aead048becbc854bee7c33900
Krasin1 <dunaevv84@gmail.com>
2025-10-05 16:27:40 +0300

    nvim markdown preview fix + new bind

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,7 +1,20 @@
--- install without yarn or npm
 return {
+  {
     "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    ft = "markdown",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    lazy = true,
+    keys = { { "<leader>md", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview Toggle" } },
+    config = function()
+      vim.g.mkdp_auto_close = true
+      vim.g.mkdp_open_to_the_world = false
+      vim.g.mkdp_open_ip = "127.0.0.1"
+      vim.g.mkdp_port = "8888"
+      vim.g.mkdp_browser = ""
+      vim.g.mkdp_echo_preview_url = true
+      vim.g.mkdp_page_title = "${name}"
+    end,
+  },
+
 }
