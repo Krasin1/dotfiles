4e0abf449e9a804d5ad9a67d9d0bcd781a2d6159
Krasin1 <dunaevv84@gmail.com>
2025-10-08 12:02:23 +0300

    remove lsp mason and completions

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,133 +1,133 @@
 return {
-	{
-		"Exafunction/codeium.nvim",
-		event = "InsertEnter",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
-		config = function()
-			require("codeium").setup({})
-		end,
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"FelipeLema/cmp-async-path",
-			"onsails/lspkind.nvim",
-			"hrsh7th/cmp-cmdline",
-		},
-		config = function()
-			local lspkind = require("lspkind")
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
-
-			cmp.setup({
-				mapping = {
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = function()
-						if cmp.visible() then
-							cmp.close()
-						else
-							cmp.complete({})
-						end
-					end,
-					--[[ ["<C-Space>"] = cmp.mapping.complete(), ]]
-					--[[ ["<C-e>"] = cmp.mapping.close(), ]]
-					["<CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					}),
-					["<Tab>"] = function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						else
-							fallback()
-						end
-					end,
-					["<S-Tab>"] = function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						else
-							fallback()
-						end
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				sources = {
-					{ name = "codeium" },
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-					{ name = "async_path" },
-				},
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				formatting = {
-					format = lspkind.cmp_format({
-						menu = {
-							codeium = "[AI]",
-							luasnip = "[Snip]",
-							nvim_lsp = "[LSP]",
-							buffer = "[Buf]",
-							async_path = "[Path]",
-						},
-						symbol_map = { Codeium = "" },
-					}),
-				},
-				experimental = {
-					native_menu = false,
-					ghost_text = true,
-				},
-			})
-			-- `/` cmdline setup.
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-				formatting = {
-					format = lspkind.cmp_format({
-						menu = {
-							buffer = "[Buf]",
-						},
-					}),
-				},
-			})
-			-- `:` cmdline setup.
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "async_path" },
-				}, {
-					{ name = "cmdline" },
-				}),
-				formatting = {
-					format = lspkind.cmp_format({
-						menu = {
-							async_path = "[Path]",
-							cmdline = "[Cmd]",
-						},
-					}),
-				},
-			})
-		end,
-	},
+	-- {
+	-- 	"Exafunction/codeium.nvim",
+	-- 	event = "InsertEnter",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"hrsh7th/nvim-cmp",
+	-- 	},
+	-- 	config = function()
+	-- 		require("codeium").setup({})
+	-- 	end,
+	-- },
+	-- {
+	-- 	"L3MON4D3/LuaSnip",
+	-- 	dependencies = {
+	-- 		"saadparwaiz1/cmp_luasnip",
+	-- 		"rafamadriz/friendly-snippets",
+	-- 	},
+	-- },
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	dependencies = {
+	-- 		"hrsh7th/cmp-nvim-lsp",
+	-- 		"hrsh7th/cmp-buffer",
+	-- 		"hrsh7th/cmp-path",
+	-- 		"FelipeLema/cmp-async-path",
+	-- 		"onsails/lspkind.nvim",
+	-- 		"hrsh7th/cmp-cmdline",
+	-- 	},
+	-- 	config = function()
+	-- 		local lspkind = require("lspkind")
+	-- 		local cmp = require("cmp")
+	-- 		require("luasnip.loaders.from_vscode").lazy_load()
+	--
+	-- 		cmp.setup({
+	-- 			mapping = {
+	-- 				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+	-- 				["<C-f>"] = cmp.mapping.scroll_docs(4),
+	-- 				["<C-Space>"] = function()
+	-- 					if cmp.visible() then
+	-- 						cmp.close()
+	-- 					else
+	-- 						cmp.complete({})
+	-- 					end
+	-- 				end,
+	-- 				--[[ ["<C-Space>"] = cmp.mapping.complete(), ]]
+	-- 				--[[ ["<C-e>"] = cmp.mapping.close(), ]]
+	-- 				["<CR>"] = cmp.mapping.confirm({
+	-- 					behavior = cmp.ConfirmBehavior.Replace,
+	-- 					select = true,
+	-- 				}),
+	-- 				["<Tab>"] = function(fallback)
+	-- 					if cmp.visible() then
+	-- 						cmp.select_next_item()
+	-- 					else
+	-- 						fallback()
+	-- 					end
+	-- 				end,
+	-- 				["<S-Tab>"] = function(fallback)
+	-- 					if cmp.visible() then
+	-- 						cmp.select_prev_item()
+	-- 					else
+	-- 						fallback()
+	-- 					end
+	-- 				end,
+	-- 			},
+	-- 			window = {
+	-- 				completion = cmp.config.window.bordered(),
+	-- 				documentation = cmp.config.window.bordered(),
+	-- 			},
+	-- 			sources = {
+	-- 				{ name = "codeium" },
+	-- 				{ name = "nvim_lsp" },
+	-- 				{ name = "luasnip" },
+	-- 				{ name = "buffer" },
+	-- 				{ name = "async_path" },
+	-- 			},
+	-- 			snippet = {
+	-- 				expand = function(args)
+	-- 					require("luasnip").lsp_expand(args.body)
+	-- 				end,
+	-- 			},
+	-- 			formatting = {
+	-- 				format = lspkind.cmp_format({
+	-- 					menu = {
+	-- 						codeium = "[AI]",
+	-- 						luasnip = "[Snip]",
+	-- 						nvim_lsp = "[LSP]",
+	-- 						buffer = "[Buf]",
+	-- 						async_path = "[Path]",
+	-- 					},
+	-- 					symbol_map = { Codeium = "" },
+	-- 				}),
+	-- 			},
+	-- 			experimental = {
+	-- 				native_menu = false,
+	-- 				ghost_text = true,
+	-- 			},
+	-- 		})
+	-- 		-- `/` cmdline setup.
+	-- 		cmp.setup.cmdline({ "/", "?" }, {
+	-- 			mapping = cmp.mapping.preset.cmdline(),
+	-- 			sources = {
+	-- 				{ name = "buffer" },
+	-- 			},
+	-- 			formatting = {
+	-- 				format = lspkind.cmp_format({
+	-- 					menu = {
+	-- 						buffer = "[Buf]",
+	-- 					},
+	-- 				}),
+	-- 			},
+	-- 		})
+	-- 		-- `:` cmdline setup.
+	-- 		cmp.setup.cmdline(":", {
+	-- 			mapping = cmp.mapping.preset.cmdline(),
+	-- 			sources = cmp.config.sources({
+	-- 				{ name = "async_path" },
+	-- 			}, {
+	-- 				{ name = "cmdline" },
+	-- 			}),
+	-- 			formatting = {
+	-- 				format = lspkind.cmp_format({
+	-- 					menu = {
+	-- 						async_path = "[Path]",
+	-- 						cmdline = "[Cmd]",
+	-- 					},
+	-- 				}),
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,174 +1,174 @@
-local signature_opts = {
-	bind = true,
-	hint_enable = false,
-	max_height = 8,
-	floating_window_off_y = -1,
-	handler_opts = {
-		border = "rounded",
-	},
-}
-
-local on_attach = function(client, bufnr)
-	if client.server_capabilities.documentSymbolProvider then
-		require("nvim-navic").attach(client, bufnr)
-	end
-	require("lsp_signature").on_attach(signature_opts, bufnr)
-
-	local function buf_set_keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
-	end
-
-	-- local function buf_set_option(...)
-	-- 	vim.api.nvim_buf_set_option(bufnr, ...)
-	-- end
-
-	-- Enable completion triggered by <c-x><c-o>
-	-- buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-	vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-	-- Mappings.
-	local opts = { noremap = true, silent = true }
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-
-	buf_set_keymap("n", "\\wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "\\wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "\\wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-
-	buf_set_keymap("n", "\\D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-
-	buf_set_keymap("n", "\\q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-
-	-- Lspsaga mappings
-	-- local map = vim.api.nvim_set_keymap
-	buf_set_keymap("n", "\\rn", "<cmd>Lspsaga rename<cr>", opts)
-	buf_set_keymap("n", "\\ca", "<cmd>Lspsaga code_action<cr>", opts)
-	buf_set_keymap("x", "\\ca", "<c-u>Lspsaga range_code_action<cr>", opts)
-	buf_set_keymap("n", "\\gh", "<cmd>Lspsaga finder<cr>", opts)
-	buf_set_keymap("n", "\\pd", "<cmd>Lspsaga peek_definition<cr>", opts)
-	buf_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
-	buf_set_keymap("n", "\\l", "<cmd>Lspsaga show_line_diagnostics<cr>", opts)
-	buf_set_keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>", opts)
-	buf_set_keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opts)
-end
-
+-- local signature_opts = {
+-- 	bind = true,
+-- 	hint_enable = false,
+-- 	max_height = 8,
+-- 	floating_window_off_y = -1,
+-- 	handler_opts = {
+-- 		border = "rounded",
+-- 	},
+-- }
+--
+-- local on_attach = function(client, bufnr)
+-- 	if client.server_capabilities.documentSymbolProvider then
+-- 		require("nvim-navic").attach(client, bufnr)
+-- 	end
+-- 	require("lsp_signature").on_attach(signature_opts, bufnr)
+--
+-- 	local function buf_set_keymap(...)
+-- 		vim.api.nvim_buf_set_keymap(bufnr, ...)
+-- 	end
+--
+-- 	-- local function buf_set_option(...)
+-- 	-- 	vim.api.nvim_buf_set_option(bufnr, ...)
+-- 	-- end
+--
+-- 	-- Enable completion triggered by <c-x><c-o>
+-- 	-- buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- 	vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+--
+-- 	-- Mappings.
+-- 	local opts = { noremap = true, silent = true }
+-- 	-- See `:help vim.lsp.*` for documentation on any of the below functions
+-- 	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+-- 	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+-- 	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+-- 	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+--
+-- 	buf_set_keymap("n", "\\wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+-- 	buf_set_keymap("n", "\\wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+-- 	buf_set_keymap("n", "\\wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+--
+-- 	buf_set_keymap("n", "\\D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+--
+-- 	buf_set_keymap("n", "\\q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+--
+-- 	-- Lspsaga mappings
+-- 	-- local map = vim.api.nvim_set_keymap
+-- 	buf_set_keymap("n", "\\rn", "<cmd>Lspsaga rename<cr>", opts)
+-- 	buf_set_keymap("n", "\\ca", "<cmd>Lspsaga code_action<cr>", opts)
+-- 	buf_set_keymap("x", "\\ca", "<c-u>Lspsaga range_code_action<cr>", opts)
+-- 	buf_set_keymap("n", "\\gh", "<cmd>Lspsaga finder<cr>", opts)
+-- 	buf_set_keymap("n", "\\pd", "<cmd>Lspsaga peek_definition<cr>", opts)
+-- 	buf_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
+-- 	buf_set_keymap("n", "\\l", "<cmd>Lspsaga show_line_diagnostics<cr>", opts)
+-- 	buf_set_keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>", opts)
+-- 	buf_set_keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opts)
+-- end
+--
 return {
-	{
-		"nvimdev/lspsaga.nvim",
-		config = function()
-			require("lspsaga").setup({
-				-- code_action = { num_shortcut = false },
-				symbol_in_winbar = { enable = false },
-				outline = { enable = false },
-				implement = { enable = false },
-				callhierarchy = { enable = false },
-				-- finder = { enable = false },
-				-- definition = { },
-			})
-		end,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter", -- optional
-			"nvim-tree/nvim-web-devicons", -- optional
-		},
-	},
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "clangd", "pyright", "lua_ls", "rust_analyzer", "ts_ls", "jdtls", "bashls" },
-			})
-			require("mason-tool-installer").setup({
-				ensure_installed = { "clang-format", "stylua", "prettier", "black", "isort", "beautysh" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"nvimdev/lspsaga.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			vim.lsp.config('lua_ls', {
-				on_attach = on_attach,
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						runtime = {
-							-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-							version = "LuaJIT",
-							-- -- Setup your lua path
-							-- path = runtime_path,
-						},
-						diagnostics = {
-							-- Get the language server to recognize the `vim` global
-							globals = { "vim" },
-						},
-						workspace = {
-							-- Make the server aware of Neovim runtime files
-							library = vim.api.nvim_get_runtime_file("", true),
-							checkThirdParty = false,
-						},
-						-- Do not send telemetry data containing a randomized but unique identifier
-						telemetry = {
-							enable = false,
-						},
-					},
-				},
-			})
-			vim.lsp.config('pyright', {
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
-			vim.lsp.config('clangd', {
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
-			vim.lsp.config('rust_analyzer', {
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
-			vim.lsp.config('jdtls', {
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
-			vim.lsp.config('ts_ls', {
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
-			vim.lsp.config('bashls', {
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
-		end,
-	},
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "InsertEnter",
-		opts = signature_opts,
-		config = function(_, opts)
-			require("lsp_signature").setup(opts)
-		end,
-	},
+-- 	{
+-- 		"nvimdev/lspsaga.nvim",
+-- 		config = function()
+-- 			require("lspsaga").setup({
+-- 				-- code_action = { num_shortcut = false },
+-- 				symbol_in_winbar = { enable = false },
+-- 				outline = { enable = false },
+-- 				implement = { enable = false },
+-- 				callhierarchy = { enable = false },
+-- 				-- finder = { enable = false },
+-- 				-- definition = { },
+-- 			})
+-- 		end,
+-- 		dependencies = {
+-- 			"nvim-treesitter/nvim-treesitter", -- optional
+-- 			"nvim-tree/nvim-web-devicons", -- optional
+-- 		},
+-- 	},
+-- 	{
+-- 		"williamboman/mason.nvim",
+-- 		config = function()
+-- 			require("mason").setup({
+-- 				ui = {
+-- 					icons = {
+-- 						package_installed = "✓",
+-- 						package_pending = "➜",
+-- 						package_uninstalled = "✗",
+-- 					},
+-- 				},
+-- 			})
+-- 		end,
+-- 	},
+-- 	{
+-- 		"williamboman/mason-lspconfig.nvim",
+-- 		dependencies = {
+-- 			"WhoIsSethDaniel/mason-tool-installer.nvim",
+-- 		},
+-- 		config = function()
+-- 			require("mason-lspconfig").setup({
+-- 				ensure_installed = { "clangd", "pyright", "lua_ls", "rust_analyzer", "ts_ls", "jdtls", "bashls" },
+-- 			})
+-- 			require("mason-tool-installer").setup({
+-- 				ensure_installed = { "clang-format", "stylua", "prettier", "black", "isort", "beautysh" },
+-- 			})
+-- 		end,
+-- 	},
+-- 	{
+-- 		"neovim/nvim-lspconfig",
+-- 		dependencies = {
+-- 			"nvimdev/lspsaga.nvim",
+-- 			"hrsh7th/cmp-nvim-lsp",
+-- 		},
+-- 		config = function()
+-- 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+-- 			vim.lsp.config('lua_ls', {
+-- 				on_attach = on_attach,
+-- 				capabilities = capabilities,
+-- 				settings = {
+-- 					Lua = {
+-- 						runtime = {
+-- 							-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+-- 							version = "LuaJIT",
+-- 							-- -- Setup your lua path
+-- 							-- path = runtime_path,
+-- 						},
+-- 						diagnostics = {
+-- 							-- Get the language server to recognize the `vim` global
+-- 							globals = { "vim" },
+-- 						},
+-- 						workspace = {
+-- 							-- Make the server aware of Neovim runtime files
+-- 							library = vim.api.nvim_get_runtime_file("", true),
+-- 							checkThirdParty = false,
+-- 						},
+-- 						-- Do not send telemetry data containing a randomized but unique identifier
+-- 						telemetry = {
+-- 							enable = false,
+-- 						},
+-- 					},
+-- 				},
+-- 			})
+-- 			vim.lsp.config('pyright', {
+-- 				on_attach = on_attach,
+-- 				capabilities = capabilities,
+-- 			})
+-- 			vim.lsp.config('clangd', {
+-- 				on_attach = on_attach,
+-- 				capabilities = capabilities,
+-- 			})
+-- 			vim.lsp.config('rust_analyzer', {
+-- 				on_attach = on_attach,
+-- 				capabilities = capabilities,
+-- 			})
+-- 			vim.lsp.config('jdtls', {
+-- 				on_attach = on_attach,
+-- 				capabilities = capabilities,
+-- 			})
+-- 			vim.lsp.config('ts_ls', {
+-- 				on_attach = on_attach,
+-- 				capabilities = capabilities,
+-- 			})
+-- 			vim.lsp.config('bashls', {
+-- 				on_attach = on_attach,
+-- 				capabilities = capabilities,
+-- 			})
+-- 		end,
+-- 	},
+-- 	{
+-- 		"ray-x/lsp_signature.nvim",
+-- 		event = "InsertEnter",
+-- 		opts = signature_opts,
+-- 		config = function(_, opts)
+-- 			require("lsp_signature").setup(opts)
+-- 		end,
+-- 	},
 }
