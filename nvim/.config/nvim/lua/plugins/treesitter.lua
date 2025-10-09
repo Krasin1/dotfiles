eec755262fcae0bc219de174eb1cb24945b6b235
Krasin1 <dunaevv84@gmail.com>
2025-10-09 17:31:54 +0300

    nvim treesitter remove comments

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -22,29 +22,3 @@ return {
 		})
 	end,
 }
-
--- return {
--- 	{
--- 		"nvim-treesitter/nvim-treesitter",
--- 		dependencies = {
--- 			"RRethy/nvim-treesitter-endwise",
--- 		},
---         branch = 'main',
--- 		build = ":TSUpdate",
--- 		config = function()
--- 			require("nvim-treesitter.configs").setup({
--- 				ensure_installed = "all",
--- 				highlight = {
--- 					enable = true,
--- 					additional_vim_regex_highlighting = false,
--- 				},
--- 				indent = {
--- 					enable = true,
--- 				},
--- 				endwise = {
--- 					enable = true,
--- 				},
--- 			})
--- 		end,
--- 	},
--- }
