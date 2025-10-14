1241d3b7535e85e4d7c90d0bfbe70098c4fb79e6
Krasin1 <dunaevv84@gmail.com>
2025-10-14 22:14:23 +0300

    nvim add highlight when yanking

--- nvim/.config/nvim/lua/lsp_features.lua
+++ nvim/.config/nvim/lua/lsp_features.lua
@@ -78,3 +78,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end
 	end,
 })
+
+-- Highlight when yanking
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
